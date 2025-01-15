ab2efca7178859570bebf19a1004d1413446fca2
xclusivesmash <sbumatshiane916@gmail.com>
2025-01-16 00:45:16 +0200

    Updated code lines

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,6 +5,7 @@ description: return a log message obfuscated.
 """
 import re
 from typing import List
+import logging
 
 
 def filter_datum(field: List[str], redaction: str,
@@ -22,3 +23,31 @@ def filter_datum(field: List[str], redaction: str,
         message = re.sub(f'{f}=.*?{separator}',
                          f'{f}={redaction}{separator}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ Initialize
+        Args:
+            fields (List[str]): fields to replace.
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Returns a log messages parsed in the req. fmt.
+        Args:
+            record (logging.LogRecord): log record to parse.
+        Returns:
+            log message in the required format.
+        """
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
