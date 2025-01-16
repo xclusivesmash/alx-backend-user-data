3f7f124b414a02e055e6e9d9cb867fcbd9563a31
xclusivesmash <sbumatshiane916@gmail.com>
2025-01-16 11:13:29 +0200

    Updated the code to add a new function called is_valid()

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -16,3 +16,18 @@ def hash_password(password: str) -> bytes:
     passwd_encoded = password.encode()
     hashed = bcrypt.hashpw(passwd_encoded, bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ checks password validity
+    Args:
+        hash_password (bytes): a hashed password from bcrypt.
+        password (str): input password as a string
+    Returns:
+        boolean value indicating if the 2 args match or not.
+    """
+    check = False
+    password_encoded = password.encode()
+    if bcrypt.checkpw(password_encoded, hashed_password):
+        check = True
+    return check
