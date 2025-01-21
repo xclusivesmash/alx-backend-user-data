0f5e58fed18518ec5616d492ea90bdcade51b546
xclusivesmash <sbumatshiane916@gmail.com>
2025-01-21 21:06:18 +0200

    Added forbidden handler method

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -12,6 +12,13 @@ def unauthorized() -> str:
     abort(401, description="Unauthorized")
 
 
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    """
+    abort(403, description="Forbidden")
+
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status
