6af22d619570d92b5d2b071751efd4022aeba5e8
xclusivesmash <sbumatshiane916@gmail.com>
2025-01-21 22:22:14 +0200

    Added a new authorization method

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,4 +10,17 @@ class BasicAuth(Auth):
     """ @description
     BasicAuth class definition.
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ Returns the Base64 part of the Authorization header
+        Args:
+            authorization_header (str): content of the Authorization header.
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        token = authorization_header.split(' ')[-1]
+        return token
