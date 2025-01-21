4560c182a14df23f0145dffa0b6ff536b215909e
xclusivesmash <sbumatshiane916@gmail.com>
2025-01-21 21:30:37 +0200

    Updated the require_auth method in Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -18,7 +18,22 @@ class Auth:
         Returns:
             False
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or excluded_paths == []:
+            return True
+        if path in excluded_paths:
+            return False
+        # fix slash '/' tolerance
+        for excluded_path in excluded_paths:
+            if excluded_path.startswith(path):
+                return False
+            elif path.startswith(excluded_path):
+                return False
+            elif excluded_path[-1] == "*":
+                if path.startswith(excluded_path[:-1]):
+                    return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Handles the request method.
