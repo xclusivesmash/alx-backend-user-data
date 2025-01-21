9071b66c353014400f79d896f5e67b41a75410f3
xclusivesmash <sbumatshiane916@gmail.com>
2025-01-21 22:11:28 +0200

    Created BasicAuth class

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+module: basic_auth
+description: handles authorization.
+"""
+from auth import Auth
+
+
+class BasicAuth(Auth):
+    """ @description
+    BasicAuth class definition.
+    """
+    pass
