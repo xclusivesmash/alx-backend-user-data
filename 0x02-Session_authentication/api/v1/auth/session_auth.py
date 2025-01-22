4d9f974c68ae3f185213b672349d404dc0fac92f
xclusivesmash <sbumatshiane916@gmail.com>
2025-01-22 21:49:13 +0200

    Updated SessionAuth class
    Added a user_id_for_session method

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -27,3 +27,14 @@ class SessionAuth(Auth):
         sessionID = str(uuid.uuid4())
         self.user_id_by_session_id[sessionID] = user_id
         return sessionID
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ returns id based on session id.
+        Args:
+            session_id (str): session id.
+        """
+        if session_id is None:
+            return None
+        if not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
