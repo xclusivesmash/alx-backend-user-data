22b62826759b334b768347809fa83b6b821246ac
xclusivesmash <sbumatshiane916@gmail.com>
2025-01-22 21:33:33 +0200

    Created a new class method for session authorization

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+module: session_auth
+description: implements the session authorization.
+"""
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """ @description.
+    SessionAuth class definition.
+    """
+    pass
