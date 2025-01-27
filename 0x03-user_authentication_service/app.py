d047603aae5eac1c705aaba22ec2a63581efb12a
xclusivesmash <sbumatshiane916@gmail.com>
2025-01-27 11:46:01 +0200

    Added a login route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -34,5 +34,21 @@ def users() -> str:
         return jsonify({"message": "email already registered"}), 400
 
 
+@app.route("/sessions", methods=['POST'])
+def login() -> str:
+    """ implements: POST /sessions
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if not (AUTH.valid_login(email, password)):
+        abort(401)
+    else:
+        # generate a new session
+        session_id = AUTH.create_session(email)
+        res = jsonify({"email": email, "message": "logged in"})
+        res.set_cookie("session_id", session_id)
+    return res
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
